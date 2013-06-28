053d41616b5d6a35c6b4a6f8c05627f7800cd631
Dan Mall <dan@danielmall.com>
2013-06-28 08:22:19 -0400

    Proper meta viewport

--- bin/-/_inc/meta.php
+++ bin/-/_inc/meta.php
@@ -1,7 +1,9 @@
 <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width" />
+
+    <!-- http://blog.javierusobiaga.com/stop-using-the-viewport-tag-until-you-know-ho -->
+    <meta name="viewport" content="width=device-width;initial-scale=1" />
 
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
