6f047b1997635bf1d5d7048c0328dfc8aa5d8995
Dan Mall <dan@superfriend.ly>
2014-12-23 00:58:47 -0500

    Set up Grunt

---
+++ GruntFile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+
+	// Project configuration
+	grunt.initConfig({
+
+		// Dependencies
+		pkg: grunt.file.readJSON('package.json'),
+
+		/*embed: {
+			site: {
+				files: {
+					'index.php': 'index.uncompressed.php',
+					'terms/index.php': 'terms/index.uncompressed.php'
+				}
+			}
+		},*/
+		sass: {                              // Task
+			dist: {                            // Target
+				options: {                       // Target options
+					outputStyle: 'compressed'
+				},
+			  	files: {                         // Dictionary of files
+			    	'-/c/main.css'			: 	'-/c/_scss/main.scss'       // 'destination': 'source'
+			    	//'/-/c/littleboxchallenge-enhanced.css'	: 	'www/-/c/_scss/enhanced.scss',       // 'destination': 'source'
+			    	//'/-/c/ie.css'			: 	'www/-/c/_scss/ie.scss'       // 'destination': 'source'
+			  	}
+			}
+		},
+
+		/*concat: {   
+		    dist: {
+		        src: [
+		        	'www/-/j/libs/*.js',
+		            'www/-/j/_source/*.js' // All JS in the _source folder		            
+		            //'js/global.js'  // This specific file
+		        ],
+		        dest: 'www/-/j/littleboxchallenge.js'
+		    }
+		},*/
+
+		/*uglify: {
+		    build: {
+		        src: 'www/-/j/littleboxchallenge.js',
+		        dest: 'www/-/j/littleboxchallenge.min.js'
+		    }
+		},*/
+
+		watch: {
+			sass: { 
+				files: ['-/c/_scss/*.scss'],
+				//tasks: ['sass', 'embed']
+				tasks: ['sass']
+			}
+			/*scripts: {
+		        files: ['www/-/j/_source/*.js'],
+		        //tasks: ['concat', 'uglify', 'embed'],
+		        options: {
+		            spawn: false,
+		        },
+		    },
+		    html: {
+		    	files: ['index.uncompressed.php', 'terms/index.uncompressed.php'],
+		    	tasks: ['embed']
+		    }*/
+		}
+	});
+
+	// Tasks
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-concat');
+	//grunt.loadNpmTasks('grunt-embed');
+
+	// Task configuration
+	//grunt.registerTask('default', ['sass', 'concat', 'uglify', 'embed', 'watch']);
+	//grunt.registerTask('default', ['sass', 'embed', 'watch']);
+	grunt.registerTask('default', ['sass', 'watch']);
+
+}
