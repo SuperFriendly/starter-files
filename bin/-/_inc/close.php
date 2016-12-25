b1543fd4f0bc3e97a89ed8cb0484ada0f8bd5ee2
Dan Mall <dan@superfriend.ly>
2016-12-25 07:41:38 -0500

    Setting up Grunt

--- bin/-/_inc/close.php
+++
@@ -1,4 +0,0 @@
-<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/jsReference.php"); echo "\n"; ?>        
-
-</body>
-</html>
--- bin/-/_inc/cssReference.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" href="/-/c/base.css" />
-    <!-- <script>document.write('<link rel="stylesheet" href="/-/c/enhanced.css" />');</script> -->
